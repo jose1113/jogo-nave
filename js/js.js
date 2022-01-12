85dcbc260f532abf1ad61f61bcf015eb43f47031
jose1113 <joseigor113@gmail.com>
2022-01-12 12:56:08 -0300

    colisao adicionada

--- js/js.js
+++ js/js.js
@@ -42,6 +42,7 @@ function start() {
         moveinimigo1()
         moveinimigo2()
         moveamigo()
+        colisao()
 	
 	} // Fim da função loop()
 
@@ -146,4 +147,12 @@ function start() {
             }
         } // Fecha executaDisparo()
     } // Fecha disparo()
+
+    function colisao() {
+        var colisao1 = ($("#jogador").collision($("#inimigo1")))
+        // jogador com o inimigo1
+
+        console.log(colisao1)
+    
+    } //Fim da função colisao()
 }
