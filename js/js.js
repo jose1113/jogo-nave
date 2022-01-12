882ab248991ea39b2aca6a6dcc3c099b4bec6ac1
jose1113 <joseigor113@gmail.com>
2022-01-12 12:52:30 -0300

    Tiro adicionado

--- js/js.js
+++ js/js.js
@@ -7,6 +7,7 @@ function start() {
 	$("#fundoGame").append("<div id='inimigo2'></div>")
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>")
 
+    var podeAtirar = true
 	var jogo = {}
     var velocidade = 5
     var posicaoY = parseInt(Math.random() * 334)
@@ -39,6 +40,8 @@ function start() {
         movefundo()
         movejogador()
         moveinimigo1()
+        moveinimigo2()
+        moveamigo()
 	
 	} // Fim da função loop()
 
@@ -73,7 +76,8 @@ function start() {
         }
         
         if (jogo.pressionou[TECLA.D]) {
-            //Chama função Disparo	
+            //Chama função Disparo
+            disparo()
         }
     
     } // fim da função movejogador()
@@ -90,4 +94,56 @@ function start() {
             $("#inimigo1").css("top", posicaoY)
         }
     } //Fim da função moveinimigo1()
+
+    function moveinimigo2() {
+        posicaoX = parseInt($("#inimigo2").css("left"))
+        $("#inimigo2").css("left", posicaoX-3)
+                    
+        if (posicaoX<=0) {  
+            $("#inimigo2").css("left", 775)
+        }
+    } // Fim da função moveinimigo2()
+
+    function moveamigo() {
+    posicaoX = parseInt($("#amigo").css("left"))
+    $("#amigo").css("left", posicaoX+1)
+      
+        if (posicaoX > 906) {
+            
+            $("#amigo").css("left", 0)
+                    
+        }
+    } // fim da função moveamigo()
+
+    
+    function disparo() {
+        
+        if (podeAtirar == true) {
+            
+        podeAtirar = false
+        
+        topo = parseInt($("#jogador").css("top"))
+        posicaoX= parseInt($("#jogador").css("left"))
+        tiroX = posicaoX + 190
+        topoTiro = topo + 37
+        $("#fundoGame").append("<div id='disparo'></div")
+        $("#disparo").css("top", topoTiro)
+        $("#disparo").css("left", tiroX)
+        
+        var tempoDisparo=window.setInterval(executaDisparo, 30)
+        
+        } //Fecha podeAtirar
+    
+        function executaDisparo() {
+            posicaoX = parseInt($("#disparo").css("left"))
+            $("#disparo").css("left", posicaoX+15)
+
+            if (posicaoX>900) {   
+                window.clearInterval(tempoDisparo)
+                tempoDisparo = null
+                $("#disparo").remove()
+                podeAtirar = true
+            }
+        } // Fecha executaDisparo()
+    } // Fecha disparo()
 }
